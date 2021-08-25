3eb2ff52a58a52fbbad7d61c125d8197d5f99f9c
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-08-26 00:57:34 +0300

    Initialized the start of program

--- src/main/java/com/PlayJava.java
+++ src/main/java/com/PlayJava.java
@@ -4,11 +4,11 @@ public class PlayJava {
 
     public static void main(String[] args) {
         // Initialization
-        //View view = new View();
-        //Model model = new Model();
-        //Controller controller = new Controller(model, view);
+        View view = new View();
+        Model model = new Model();
+        Controller controller = new Controller(model, view);
         // Run
-        //controller.processUser();
+        controller.processUser();
     }
 
 }
