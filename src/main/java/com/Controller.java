36f205a31def4398f7ab75b6554b3daa62eb551f
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-08-26 00:06:55 +0300

    Init constructor and main fields

---
+++ src/main/java/com/Controller.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Controller {
+
+    // Constructor
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model  = model;
+        this.view = view;
+    }
+
+
+
+}
