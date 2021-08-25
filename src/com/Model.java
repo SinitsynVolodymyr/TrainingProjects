adeebeeaf7fdffb21733426bd0c17839cc708d65
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-08-25 23:21:00 +0300

    Added main type of data

---
+++ src/com/Model.java
@@ -0,0 +1,15 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    public static final int RAND_MAX = 100;
+
+    private int value;
+    private List<Integer> historyNumberList = new ArrayList<>();
+    private int minValue = 0;
+    private int maxValue = RAND_MAX;
+
+
+}
