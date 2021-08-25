02d979924a4ed76e4dada8c37e39dd3c50295ca4
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-08-26 00:54:42 +0300

    Corrected one of constant's name

--- src/main/java/com/View.java
+++ src/main/java/com/View.java
@@ -3,9 +3,9 @@ package com;
 public class View {
     // Text's constants
     public static final String INPUT_INT_DATA = "Input INT value = ";
-    public static final String WRONG_INT_RANGE = "Wrong input! Repeat please!";
+    public static final String WRONG_INT_DATA = "Wrong input! Repeat please!";
     public static final String SEPARATOR = "\n";
-    public static final String WRONG_INT_DATA = "The number isn't included in the range";
+    public static final String WRONG_INT_RANGE = "The number isn't included in the range";
     public static final String SUCCESS_INT_DATA = "You guessed the number!";
     public static final String NUMBER_IS_LESS = "Your number is less than needed";
     public static final String NUMBER_IS_BIGGER = "Your number is more than needed";
