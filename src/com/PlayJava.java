5be421c98804638bb95a153552c0486952d2aa10
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-08-25 22:05:09 +0300

    Initial commit

---
+++ src/com/PlayJava.java
@@ -0,0 +1,14 @@
+package com;
+
+public class PlayJava {
+
+    public static void main(String[] args) {
+        // Initialization
+        //View view = new View();
+        //Model model = new Model();
+        //Controller controller = new Controller(model, view);
+        // Run
+        //controller.processUser();
+    }
+
+}
