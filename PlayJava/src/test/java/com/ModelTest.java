372f9c25b82293dbfe80520907a313d2d522d251
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-08-30 16:36:34 +0300

    Added the checkValue test

--- PlayJava/src/test/java/com/ModelTest.java
+++ PlayJava/src/test/java/com/ModelTest.java
@@ -1,16 +1,32 @@
 package com;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
+    private static Map<Integer, Model.ComparisonResult> results = new HashMap<>();
+    private static int minCheckValue = -100;
+    private static int maxCheckValue = 200;
+    private static Model model ;
+
 
     private int minRandValue = 3;
     private int maxRandValue = 5;
+
+    @BeforeAll
+    static void initResults(){
+        model = new Model();
+        for (int i = minCheckValue; i<=maxCheckValue; i++){
+            results.put(i, model.checkValue(i));
+        }
+    }
 
     @Test
     void rand() {
@@ -47,4 +63,72 @@ class ModelTest {
         }
 
     }
+
+    @Test
+    void checkValue_NOTNULL() {
+        assertFalse(results.values().contains(null));
+    }
+
+    @Test
+    void checkValue_OUT_OF_RANGE() {
+        assertTrue(results.values().contains(
+                Model.ComparisonResult.OVER_THE_RANGE));
+
+        for (Map.Entry<Integer,
+                Model.ComparisonResult> entry : results.entrySet()) {
+            if (entry.getKey()<=0||entry.getKey()>=Model.RAND_MAX){
+                assertEquals(entry.getValue(), Model.ComparisonResult.OVER_THE_RANGE);
+            }
+        }
+    }
+
+    @Test
+    void checkValue_IN_RANGE() {
+        assertTrue(results.values().contains(
+                Model.ComparisonResult.IS_BIGGEST));
+        assertTrue(results.values().contains(
+                Model.ComparisonResult.IS_LOWEST));
+
+        boolean isFoundRightValue = false;
+        for (Map.Entry<Integer,
+                Model.ComparisonResult> entry : results.entrySet()) {
+
+            if (entry.getKey()>0&&entry.getKey()<Model.RAND_MAX){
+                boolean isBiggest = entry.getValue().equals(Model.ComparisonResult.IS_BIGGEST);
+                boolean isLowest = entry.getValue().equals(Model.ComparisonResult.IS_LOWEST);
+
+                if (entry.getValue().equals(Model.ComparisonResult.RIGHT_NUMBER)){
+                    isFoundRightValue = true;
+                    continue;
+                }
+
+                if (isFoundRightValue){
+                    assertTrue(isBiggest);
+                    assertFalse(isLowest);
+                }else{
+                    assertTrue(isLowest);
+                    assertFalse(isBiggest);
+                }
+            }
+        }
+    }
+
+    @Test
+    void checkValue_RIGHT_VALUE() {
+        assertTrue(results.values().contains(
+                Model.ComparisonResult.RIGHT_NUMBER));
+
+        boolean isFoundRightValue = false;
+        for (Map.Entry<Integer,
+                Model.ComparisonResult> entry : results.entrySet()) {
+            if (entry.getValue().equals(
+                    Model.ComparisonResult.RIGHT_NUMBER)) {
+                assertTrue(entry.getKey() > 0 && entry.getKey() < Model.RAND_MAX);
+                assertFalse(isFoundRightValue);
+                isFoundRightValue = true;
+            }
+        }
+    }
+
+
 }
