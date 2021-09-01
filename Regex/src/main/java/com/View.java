4d284d1d7a7a87ebc63c8388ae6d8068f01cee26
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-02 00:21:24 +0300

    Init Land enum

--- Regex/src/main/java/com/View.java
+++ Regex/src/main/java/com/View.java
@@ -1,4 +1,25 @@
 package com;
 
+import java.util.Locale;
+
 public class View {
+
+
+    enum Land{
+
+        EN(new Locale("en")),UA(new Locale("UA","ua"));
+
+        private Locale locale;
+
+        private Land(Locale locale){
+            this.locale = locale;
+        }
+
+        public Locale getLocale() {
+            return locale;
+        }
+    }
+
 }
+
+
