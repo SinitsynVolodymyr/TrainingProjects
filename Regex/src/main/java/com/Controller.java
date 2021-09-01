06033098f2abac7dc2410e8b8514f10c80b0286f
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-01 23:49:39 +0300

    init process user

--- Regex/src/main/java/com/Controller.java
+++ Regex/src/main/java/com/Controller.java
@@ -1,10 +1,22 @@
 package com;
 
+import java.util.Scanner;
+
 public class Controller {
+    private Model model;
+    private View view;
 
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
 
+    /**
+     * The user request handler method
+     */
+    public void processUser(){
+        Scanner sc = new Scanner(System.in);
 
-
-
+    }
 
 }
--- Regex/src/main/java/com/Regex.java
+++ Regex/src/main/java/com/Regex.java
@@ -3,7 +3,10 @@ package com;
 public class Regex {
 
     public static void main(String[] args) {
-
+        View view = new View();
+        Model model = new Model();
+        Controller controller = new Controller(model, view);
+        controller.processUser();
     }
 
 }
