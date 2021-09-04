dfb19f638e6de824e3c4c55d6887a7963700332c
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-04 22:31:14 +0300

    Added java-doc to Model

--- Regex/src/main/java/com/Model.java
+++ Regex/src/main/java/com/Model.java
@@ -3,10 +3,20 @@ package com;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The model class from MVC model
+ * @author SinitsynVolodymyr
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public class Model {
-
+    /** List of registered users */
     List<User> userList = new ArrayList<>();
 
+    /**
+     * Add user object to model
+     * @param user - The added user
+     */
     public void addUser(User user){
         userList.add(user);
     }
