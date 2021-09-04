5064e6221737fbc19464941dea2f1474a0c48d86
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-04 22:27:40 +0300

    Added java-doc to Controller

--- Regex/src/main/java/com/Controller.java
+++ Regex/src/main/java/com/Controller.java
@@ -5,11 +5,25 @@ import com.view.View;
 
 import java.util.Scanner;
 
+/**
+ * The controller class from MVC model
+ * @author SinitsynVolodymyr
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public class Controller {
+    /** The model object from MVC model */
     private Model model;
+    /** The view object from MVC model */
     private View view;
+    /** The object that processes registration*/
     private RegistrationHandler regHandler;
 
+    /**
+     * Constructor of controller
+     * @param model - The model object from MVC model
+     * @param view - The view object from MVC model
+     */
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
@@ -17,7 +31,8 @@ public class Controller {
     }
 
     /**
-     * The user request handler method
+     * The user request handler method.
+     * This method creates user object and adds this user in MVC model
      */
     public void processUser(){
         Scanner sc = new Scanner(System.in);
