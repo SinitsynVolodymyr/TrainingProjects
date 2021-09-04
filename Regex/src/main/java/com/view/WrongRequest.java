277c43531e5feb50736b1c8e653c837152202d89
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-04 22:11:31 +0300

    Added java-doc to WrongRequest

--- Regex/src/main/java/com/view/WrongRequest.java
+++ Regex/src/main/java/com/view/WrongRequest.java
@@ -1,15 +1,31 @@
 package com.view;
 
+/** The object that characterizes a wrong request massage
+ * @author SinitsynVolodymyr
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public enum WrongRequest {
 
+    /** Key of wrong request */
     WRONG_REQUEST("input.wrong"),;
 
+    /** The bundle key */
     String keys;
 
+    /**
+     * Constructor of WrongRequest
+     * @param keys - key of request error
+     */
     WrongRequest(String keys) {
         this.keys = keys;
     }
 
+    /**
+     * Gets the bundle key
+     * @return - the wrong message of bundle key
+     *
+     */
     public String getKey() {
         return keys;
     }
