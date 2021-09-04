c6770c9e16479bf4e69647dfbe9101643af3bb69
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-04 22:34:52 +0300

    Added java-doc to Regex

--- Regex/src/main/java/com/Regex.java
+++ Regex/src/main/java/com/Regex.java
@@ -2,8 +2,15 @@ package com;
 
 import com.view.View;
 
+/**
+ * The object of the program start
+ */
 public class Regex {
 
+    /**
+     * The program start method
+     * @param args - system arguments (are ignored in this program)
+     */
     public static void main(String[] args) {
         View view = new View(View.Land.UA);
         Model model = new Model();
