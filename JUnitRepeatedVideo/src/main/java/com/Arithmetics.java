334b6b8020301cc0cdfa29bd0437474b92508fd5
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-05 23:24:10 +0300

    Added Arithmetics

---
+++ JUnitRepeatedVideo/src/main/java/com/Arithmetics.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Arithmetics {
+
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    public double deduct(double a, double b){
+        return a - b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+
+    public float div(float a, float b){
+        return  a / b;
+    }
+
+}
