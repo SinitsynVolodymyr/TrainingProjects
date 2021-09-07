dd93a7e5f71793bc5a187d5e043cfa27982674ac
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-07 22:53:22 +0300

    Added login handler and transport build user to RegistrationHandler

--- Regex/src/main/java/com/controller/Controller.java
+++ Regex/src/main/java/com/controller/Controller.java
@@ -2,8 +2,10 @@ package com.controller;
 
 import com.model.Model;
 import com.model.User;
+import com.model.database.DBNoteBook;
 import com.view.MessageRequest;
 import com.view.View;
+import com.view.WrongRequest;
 
 import java.util.Scanner;
 
@@ -38,31 +40,7 @@ public class Controller {
      */
     public void processUser(){
         Scanner sc = new Scanner(System.in);
-        User user = new User();
-
-
-        user.setName(regHandler.input(sc,MessageRequest.INPUT_FIRSTNAME_FORMAT));
-        user.setLogin(regHandler.input(sc,MessageRequest.INPUT_LOGIN_FORMAT));
-
-
-        /* //Disable unimportant fields
-        user.setSurname(regHandler.input(sc,MessageRequest.INPUT_SURNAME_FORMAT));
-        user.setPatronymic(regHandler.input(sc,MessageRequest.INPUT_PATRONYMIC_FORMAT));
-        user.setNick(regHandler.input(sc,MessageRequest.INPUT_NICK_FORMAT));
-        user.setComment(regHandler.input(sc,MessageRequest.INPUT_COMMENT_FORMAT));
-        user.setGroupName(regHandler.input(sc,MessageRequest.INPUT_GROUP_FORMAT));
-        user.setHomePhone(regHandler.input(sc,MessageRequest.INPUT_HOMEPHONE_FORMAT));
-        user.setMobPhone(regHandler.input(sc,MessageRequest.INPUT_MOBPHONE_FORMAT));
-        user.setSecMobPhone(regHandler.input(sc,MessageRequest.INPUT_SECPHONE_FORMAT));
-        user.setEmail(regHandler.input(sc,MessageRequest.INPUT_EMAIL_FORMAT));
-        user.setSkype(regHandler.input(sc,MessageRequest.INPUT_SKYPE_FORMAT));
-        user.setCity(regHandler.input(sc,MessageRequest.INPUT_CITY_FORMAT));
-        user.setAddressIndex(regHandler.input(sc,MessageRequest.INPUT_INDEX_FORMAT));
-        user.setAddressStreet(regHandler.input(sc,MessageRequest.INPUT_STREET_FORMAT));
-        user.setAddressHouseNum(regHandler.input(sc,MessageRequest.INPUT_HOUSENUM_FORMAT));
-        user.setAddressFlatNum(regHandler.input(sc,MessageRequest.INPUT_FLATNUM_FORMAT));
-        */
-
+        User user = regHandler.registerUser(sc);
         model.getNoteBook().addUser(user);
     }
 
