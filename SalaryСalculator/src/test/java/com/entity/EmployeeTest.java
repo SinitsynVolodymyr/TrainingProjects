416aeb19f9e18927f5d2028f7b08eccd3b835cfe
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-11 23:42:30 +0300

    Created a EmployeeTest

--- SalaryСalculator/src/test/java/com/entity/EmployeeTest.java
+++ SalaryСalculator/src/test/java/com/entity/EmployeeTest.java
@@ -1,5 +1,6 @@
 package com.entity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -8,14 +9,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    Employee natasha1;
+    Employee misha1;
+
+    Employee natasha2;
+    Employee misha2;
+
+    @BeforeEach
+    void init(){
+         natasha1 = new Employee("Natasha", new Date(1284237214), new Date(1599856414));
+         misha1 = new Employee("Misha", new Date(1315773214), new Date(1442003614));
+
+         natasha2 = new Employee("Natasha", new Date(1284237214), new Date(1599856414));
+         misha2 = new Employee("Misha", new Date(1315773214), new Date(1442003614));
+    }
+
     @Test
     void testEquals() {
-        new Employee("Natasha",new Date(1284237214), new Date(1599856414));
 
+        assertTrue(natasha1.equals(natasha1));
+        assertTrue(natasha1.equals(natasha2));
+        assertTrue(natasha2.equals(natasha1));
 
+        assertTrue(misha1.equals(misha1));
+        assertTrue(misha1.equals(misha2));
+        assertTrue(misha2.equals(misha1));
+
+        assertFalse(misha1.equals(natasha1));
+        assertFalse(natasha1.equals(misha1));
     }
 
     @Test
     void testHashCode() {
+
+        assertEquals(natasha1.hashCode(),natasha1.hashCode());
+        assertEquals(natasha1.hashCode(),natasha2.hashCode());
+        assertEquals(natasha2.hashCode(),natasha1.hashCode());
+
+        assertEquals(misha1.hashCode(),misha1.hashCode());
+        assertEquals(misha1.hashCode(),misha2.hashCode());
+        assertEquals(misha2.hashCode(),misha1.hashCode());
+
+        assertNotEquals(misha1.hashCode(),natasha1.hashCode());
+        assertNotEquals(natasha1.hashCode(),misha1.hashCode());
+
+
     }
 }
