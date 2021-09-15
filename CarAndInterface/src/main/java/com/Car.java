b7709aad3252562ca34612d2582914ca51ea04ae
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-16 01:20:40 +0300

    Implemented interfaces to Car

--- CarAndInterface/src/main/java/com/Car.java
+++ CarAndInterface/src/main/java/com/Car.java
@@ -1,13 +1,17 @@
 package com;
 
 
-public class Car {
+public class Car implements CarDriver,CarBattery,CarRefueler{
+
     public enum Transmission { MECHANICAL , AUTOMATIC };
     private Transmission transmission;
     private String model;
     private String number;
     private String vin;
     private DriverLicense.Category category;
+
+    boolean isMoveCar = false;
+    boolean isOpenFuelTankCover = false;
 
     public Car() {
     }
@@ -19,6 +23,69 @@ public class Car {
         this.vin = vin;
         this.category = category;
     }
+
+
+    @Override
+    public double getHeight() {
+        return 173;
+    }
+
+    @Override
+    public double getWidth() {
+        return 306;
+    }
+
+    @Override
+    public double getDepth() {
+        return 225;
+    }
+
+    @Override
+    public void move() {
+        if (isOpenFuelTankCover)
+            System.out.println("We can't move because fuel tank cover is open");
+        else {
+            isMoveCar = true;
+            System.out.println("The car is moving");
+        }
+    }
+
+    @Override
+    public void stop() {
+        isMoveCar = false;
+        System.out.println("The car stopped");
+    }
+
+    @Override
+    public void openFuelTankCover() {
+        if (isMoveCar)
+            System.out.println("We can't open fuel tank cover because the car is moving");
+        else {
+            isOpenFuelTankCover = true;
+            System.out.println("The fuel tank cover is open");
+        }
+    }
+
+    @Override
+    public void refuel() {
+        if (!isOpenFuelTankCover)
+            System.out.println("We can't refuel the car because the fuel tank cover is close");
+        else {
+            System.out.println("The car is refueling");
+        }
+    }
+
+    @Override
+    public void closeFuelTankCover() {
+        if (isMoveCar)
+            System.out.println("We can't close fuel tank cover because the car is moving");
+        else {
+            isOpenFuelTankCover = false;
+            System.out.println("The fuel tank cover is close");
+        }
+    }
+
+
 
     public String getModel() {
         return model;
