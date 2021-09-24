39f8adfe934d215cf08a4d5c45e4222b35626e67
SinitsynVolodymyr <SinitsynVolodymyr@gmail.com>
2021-09-24 21:08:13 +0300

    Added result of task

--- StreamInteger/src/com/StreamInteger.java
+++ StreamInteger/src/com/StreamInteger.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class StreamInteger {
@@ -8,12 +9,21 @@ public class StreamInteger {
     public static void main(String[] args) {
 
         int[] ints = generateIntegerArray();
-        IntStream intStream = Arrays.stream(ints);
+        Supplier<IntStream> intStream
+                = () -> IntStream.of(ints);
+
+        int mulNumber = 5;
+
+        System.out.println(intStream.get().average().getAsDouble());
+        System.out.println(String.valueOf(intStream.get().min().getAsInt()));
+        System.out.println(String.valueOf(intStream.get().filter(value -> value == 0).count()));
+        System.out.println(String.valueOf(intStream.get().filter(value -> value > 0).count()));
+        intStream.get().map(value -> value*mulNumber).forEach(value -> System.out.print(value+" "));
     }
 
 
     public static int[] generateIntegerArray(){
-        int[] result = {20,10,-5,6,42,-36,-123,542,22};
+        int[] result = {20,10,-5,6,0,42,-36,0,-123,542,22};
         return result;
     }
 
